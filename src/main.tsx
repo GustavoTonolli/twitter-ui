d52012b1bf0233cb3643e5a2556e06ca4724ab6f
gustavotonolli <gustavo.tonolli@hotmail.com>
2023-10-26 14:51:56 -0300

    :sparkles: twitter timeline

--- src/main.tsx
+++ src/main.tsx
@@ -1,53 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./global.css";
-import twitterLogo from "./assets/logo.svg";
-import {Bell, BookmarkSimple, DotsThreeCircle, Envelope, FileText, Hash, House, User} from "phosphor-react"
+import { Sparkle } from "phosphor-react";
+import { Tweet } from "./components/Tweet";
+import { Sidebar } from "./components/Sidebar";
+import { Header } from "./components/Header";
+import { Separator } from "./components/Separator";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <div className="layout">
-      <aside className="sidebar">
-        <img className="logo" src={twitterLogo} alt="logo" />
-        <nav className="main-navigation">
-          <a className="active" href="">
-            <House weight="fill"/>
-            Home
-          </a>
-          <a href="">
-            <Hash/>
-            Explore
-          </a>
-          <a href="">
-            <Bell/>
-            Notifications
-          </a>
-          <a href="">
-            <Envelope/>
-            Messages
-          </a>
-          <a href="">
-            <BookmarkSimple/>
-            Bookmarks
-          </a>
-          <a href="">
-            <FileText/>
-            Lists
-          </a>
-          <a href="">
-            <User/>
-            Profile
-          </a>
-          <a href="">
-            <DotsThreeCircle/>
-            More
-          </a>
-        </nav>
-        <button className="new-tweet" type="button">
-          Tweet
-        </button>
-      </aside>
-      <div className="content">content</div>
+      <Sidebar />
+      <div className="content">
+        <main className="timeline">
+          <Header title="Home" />
+
+          <form className="new-tweet-form">
+            <label htmlFor="tweet">
+              <img
+                src="https://github.com/gustavotonolli.png"
+                alt="Gustavo Tonolli"
+              />
+              <textarea
+                name="tweet"
+                id="tweet"
+                placeholder="What's happening?"
+              />
+            </label>
+            <button type="submit">Tweet</button>
+          </form>
+
+          <Separator />
+
+          <Tweet />
+          <Tweet />
+        </main>
+      </div>
     </div>
   </React.StrictMode>
 );
